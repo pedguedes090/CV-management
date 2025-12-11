c1f2c2e817fb07ee1e4afad11000fdb72b61807d
pedguedes090 <pedguedes090@outlook.com>
2025-12-11 07:44:20 +0700

    update các router

--- client/CV-management/src/pages/profileManagement/ProfileManagement.tsx
+++ client/CV-management/src/pages/profileManagement/ProfileManagement.tsx
@@ -3,7 +3,7 @@ import Header from '../../components/header_footer/Header'
 import Footer from '../../components/header_footer/Footer'
 import CVContent from '../../components/profileManagement/CVContent'
 import NavbarLogOut from '../dashboard/NavbarLogOut'
-export default function Dashboard() {
+export default function ProfileManagement() {
   return (
     <div className='w-full h-screen'>
         <Header></Header>
